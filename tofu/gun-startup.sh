d70ac6a1a608646751dfda331cbe9be28ca4e40a
Caio Borghi <caiohoborghi@gmail.com>
2023-12-14 17:47:11 -0300

    install go and node on the gun server

--- tofu/gun-startup.sh
+++ tofu/gun-startup.sh
@@ -14,3 +14,27 @@ sudo mv vegeta-temp/vegeta /usr/local/bin
 
 rm -rf vegeta.tar.gz
 rm -rf vegeta-temp
+
+# Remove existing Node.js (if installed)
+sudo apt-get remove -y nodejs
+sudo apt-get purge -y nodejs
+sudo apt-get autoremove -y
+
+# Install Node.js 21.2.0
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=21
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install nodejs -y
+echo "Installed Node!"
+node -v
+
+# Installing Go
+wget https://golang.org./dl/go1.21.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/go_path.sh
+echo "Installed go!"
+
+# Installing jagger
+go install github.com/rs/jaggr@latest
+go install github.com/rs/jplot@latest
